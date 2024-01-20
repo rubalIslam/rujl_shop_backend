268b0b10e261edbf3dab79b609ac66f3de062c8c
rubal <bedarullaskar@gmail.com>
2024-01-20 09:58:47 +0530

    first commit

--- app.js
+++ app.js
@@ -45,14 +45,15 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 })
 
 //Server
-/*
+
 app.listen(3000, ()=>{
 
     console.log('server is running http://localhost:3000');
 })
-*/
 
+/*
 var server = app.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
     console.log("Express is working on port " +port)
 })
+*/
